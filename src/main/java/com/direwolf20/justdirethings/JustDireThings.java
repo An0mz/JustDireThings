247858a874d6de95883a6ca57d1d8f8a4cdc6157
Anomz <58313106+An0mz@users.noreply.github.com>
2026-05-04 04:50:29 -0400

    Fix init error on game launch

--- src/main/java/com/direwolf20/justdirethings/JustDireThings.java
+++ src/main/java/com/direwolf20/justdirethings/JustDireThings.java
@@ -8,22 +8,24 @@ import com.direwolf20.justdirethings.setup.Registration;
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLLoader;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(JustDireThings.MODID)
 public class JustDireThings {
     public static final String MODID = "justdirethings";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public JustDireThings(IEventBus modEventBus) {
+    public JustDireThings() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
         Registration.init(modEventBus);
         Config.register();
 
         modEventBus.addListener(ModSetup::init);
         ModSetup.CREATIVE_MODE_TABS.register(modEventBus);
-        modEventBus.addListener(PacketHandler::registerNetworking); // registers on FMLCommonSetupEvent
+        modEventBus.addListener(PacketHandler::registerNetworking);
         if (FMLLoader.getDist().isClient()) {
             modEventBus.addListener(ClientSetup::init);
         }
