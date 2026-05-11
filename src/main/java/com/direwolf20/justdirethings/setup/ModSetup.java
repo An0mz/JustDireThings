c1c698c77ea5c71f142ad454d1a3df5a6dedd1a1
Anomz <58313106+An0mz@users.noreply.github.com>
2026-05-11 13:11:45 -0400

    Fix custom coal not burning

--- src/main/java/com/direwolf20/justdirethings/setup/ModSetup.java
+++ src/main/java/com/direwolf20/justdirethings/setup/ModSetup.java
@@ -10,6 +10,7 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.registries.RegistryObject;
@@ -23,6 +24,20 @@ public class ModSetup {
         MinecraftForge.EVENT_BUS.register(LivingEntityEvents.class);
         MinecraftForge.EVENT_BUS.register(PlayerEvents.class);
         MinecraftForge.EVENT_BUS.register(CapabilityEvents.class);
+        MinecraftForge.EVENT_BUS.addListener(ModSetup::registerFurnaceFuels);
+    }
+
+    public static void registerFurnaceFuels(FurnaceFuelBurnTimeEvent event) {
+        Item item = event.getItemStack().getItem();
+        if (item == Registration.Coal_T1.get())           event.setBurnTime(4800);
+        else if (item == Registration.CoalBlock_T1_ITEM.get()) event.setBurnTime(48000);
+        else if (item == Registration.Coal_T2.get())      event.setBurnTime(14400);
+        else if (item == Registration.CoalBlock_T2_ITEM.get()) event.setBurnTime(144000);
+        else if (item == Registration.Coal_T3.get())      event.setBurnTime(43200);
+        else if (item == Registration.CoalBlock_T3_ITEM.get()) event.setBurnTime(432000);
+        else if (item == Registration.Coal_T4.get())      event.setBurnTime(129600);
+        else if (item == Registration.CoalBlock_T4_ITEM.get()) event.setBurnTime(1296000);
+        else if (item == Registration.CharcoalBlock_ITEM.get()) event.setBurnTime(16000);
     }
 
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
